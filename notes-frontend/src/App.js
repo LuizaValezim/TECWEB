9a1c27a7707401bd368486ccb1b31f6b5fcd4c7f
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2021-10-28 17:46:57 -0300

    Inicio React

---
+++ notes-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Note from "./components/Note";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+        <Note />
+    </div>
+  );
+}
+
+export default App;
---
+++ notes-frontend/src/components/Note/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./index.css";
+
+export default function Note(){
+    return (
+        <div className='title'>
+            <h1> Chords </h1>
+            <h1> Theory </h1>
+        </div>
+        
+        // <div className="card">
+        //     <h3 className="card-title">Receita de miojo</h3>
+        //     <div className="card-content">
+        //         <p>
+        //         Bata com um martelo antes de abrir o pacote. Misture o tempero,
+        //         coloque em uma vasilha e aproveite seu snack :)
+        //         </p>
+        //     </div>
+        // </div>
+    );
+}
